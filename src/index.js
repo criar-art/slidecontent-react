0976e2e200a1bf05679d672b003a713c044ce473
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-14 12:34:42 -0300

    add reset css and adjustment styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
 import * as serviceWorker from './serviceWorker';
+import "./assets/_reset.scss";
 
 createRoot(document.getElementById('root')).render(<App />);
 
